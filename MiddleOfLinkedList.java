25fc499cb75a5a5e0fc4929606c3db6b0ed7b891
vivek-volante <68999032+vivek-volante@users.noreply.github.com>
2022-04-01 19:39:21 +0530

    Create MiddleOfLinkedList.java

---
+++ MiddleOfLinkedList.java
@@ -0,0 +1,84 @@
+import java.util.*;
+public class MiddleOfLinkedList {
+    public static void main(String args[]) {
+           //creating LinkedList with 5 elements including head
+      LinkedList linkedList = new LinkedList();
+      LinkedList.Node head = linkedList.head();
+      linkedList.add( new LinkedList.Node("11"));
+      linkedList.add( new LinkedList.Node("22"));
+      linkedList.add( new LinkedList.Node("33"));
+      linkedList.add( new LinkedList.Node("44"));
+      linkedList.add( new LinkedList.Node("55"));
+   
+      //finding middle element of LinkedList in single pass
+      LinkedList.Node current = head;
+      int length = 0;
+      LinkedList.Node middle = head;
+   
+      while(current.next() != null){
+          length++;
+          if(length%2 ==0){
+              middle = middle.next();
+          }
+          current = current.next();
+      }
+    
+      if(length%2 == 1){
+          middle = middle.next();
+      }
+
+      System.out.println("length of LinkedList: " + length);
+      System.out.println("middle element of LinkedList : "                                  + middle);
+     
+    }
+}
+
+
+
+class LinkedList{
+    private Node head;
+    private Node tail;
+ 
+    public LinkedList(){
+        this.head = new Node("head");
+        tail = head;
+    }
+ 
+    public Node head(){
+        return head;
+    }
+ 
+    public void add(Node node){
+        tail.next = node;
+        tail = node;
+    }
+ 
+    public static class Node{
+        private Node next;
+        private String data;
+
+        public Node(String data){
+            this.data = data;
+        }
+     
+        public String data() {
+            return data;
+        }
+
+        public void setData(String data) {
+            this.data = data;
+        }
+
+        public Node next() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+     
+        public String toString(){
+            return this.data;
+        }
+    }
+}
